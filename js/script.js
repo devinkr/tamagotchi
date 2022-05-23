049a59ffa01215c4fc25970f0ad349959881be10
Devin Raleigh <devinkr@gmail.com>
2022-05-23 16:02:31 -0600

    Add boilerplate and scaffold html

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/*----- constants -----*/
+const BABY_IMAGE = './assets/baby.webp';
+const CHILD_IMAGE = './assets/child.webp';
+const TEEN_IMAGE = './assets/teen.webp';
+const ADULT_IMAGE = './assets/adult.webp';
+const MAX_STAT_VALUE = 100;
+const MAX_BABY_AGE = 5;
+const MAX_CHILD_AGE = 12;
+const MAX_TEEN_AGE = 19;
+const HUNGER_INTERVAL = 2000;
+const SLEEPINESS_INTERVAL = 3000;
+const BOREDOM_INTERVAL = 4000;
+const AGE_INTERVAL = 5000;
+
+/*----- app's state (variables) -----*/
+let age;
+let hunger;
+let boredom;
+let sleepiness;
+
+/*----- cached element references -----*/
+// save DOM elements you access more than once as a variable
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+// init() function that will initialize game state. This can be used for starting a new game as well.
+// render() function responsible for updating DOM
